b598eb09bd993c3774912e0b128670ef0f05b042
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-05-20 00:26:22 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (47.63%) - LeetSync

--- pascals-triangle/pascals-triangle.cpp
+++ pascals-triangle/pascals-triangle.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-   long long int factorial(int n)
-    {
-        if(n==0)
-        {
-            return 1;
-        }
-        long long int ans=1;
-        for(int i=n;i>=1;i--)
-        {
-            ans*=i;
-        }
-        return ans;
-    }
+    /*LOGIC: Every element is formed by a formulae that is: nCr=(n!)/(r! * (n-r)!)
+      But there is a slight modification for this problem
+      Every element is formed by the formulae (i-1)C(j-1) where i->rows and j->coolumns
+    */
 
+    /* 
+    this is the best method to calculate the value of nCr
+    10C3= (10*9*8*7...*1)/((3*2*1)*(7*6*5*...))
+        = (10*9*8)/(3*2*1)
+     or = (10*9*8)/(1*2*3) 
+     or = (10/1)*(9/2)*(8/3)
+
+     therefore 10C3 can be calculated as (10/1)*(9/2)*(8/3)
+     thus the below function does calculates the same  
+    */
     int nCr(int n, int r)
     {
         int res=1;
