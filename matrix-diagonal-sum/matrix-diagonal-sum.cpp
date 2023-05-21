7129a30ef78a5f29c4e9d5f971c236a65cafc063
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-05-21 15:58:06 +0530

    Time: 7 ms (99.43%) | Memory: 11.1 MB (79.28%) - LeetSync

--- matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    /*
+    LOGIC: To find the principal diagonal element (i==j)
+           To find the non-principal diagonal element (i+j==n-1) 
+    */
     int diagonalSum(vector<vector<int>>& mat) {
         int n=mat.size();
         int sum=0;
