e9ca00f645ec4674d2c7f375c5bd91e9e2bb1e72
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-05-22 23:37:33 +0530

    Time: 128 ms (52.87%) | Memory: 61.2 MB (88.28%) - LeetSync

---
+++ find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+class Solution 
+{
+
+public:
+
+int findDuplicate(vector<int>& arr) 
+{
+	//step 1: initialize the slow and fast pointer
+	int slow=arr[0];
+	int fast=arr[0];
+
+	//step 2: 
+	do
+	{
+		slow=arr[slow];
+		fast=arr[arr[fast]];
+	}while(slow!=fast);
+
+    fast=arr[0];
+
+	// step 3:
+	while(slow!=fast)
+	{
+		slow=arr[slow];
+		fast=arr[fast];
+	}
+
+	return fast;
+}
+
+/*Solution :
+Step 1 : Find “min” and “max” value in the given array. It will take O(n).
+Step 2 : Find XOR of all integers from range “min” to “max” ( inclusive ).
+Step 3 : Find XOR of all elements of the given array.
+Step 4 : XOR of Step 2 and Step 3 will give the required duplicate number.
+NOTE: THIS METHOD WILL FAIL FOR [2,2,2,2,2,2,2]
+      THEREFORE THE ELEMENTS MUST IN IN RANGE (a,b)
+
+    int findDuplicate(vector<int>& arr)
+    {
+        int n=arr.size();
+
+        // step 1: find the maximum and the minimum from the array
+        int maximum=INT_MIN, minimum=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            maximum=max(arr[i], maximum);
+            minimum=min(arr[i], minimum);
+        }
+
+        // step 2: find XOR of all integers from range [minimum, maximum]
+        int minXORmax=minimum;
+        for(int i=minimum+1;i<=maximum;i++)
+        {
+            minXORmax ^= i;
+        }
+
+        // step 3: find XOR of all elements of the given array
+        int allXOR=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            allXOR ^= arr[i];
+        }
+
+        // step 4: finally find the XOR of step 2 and 3
+        int ans= minXORmax ^ allXOR;
+        
+        return ans;
+    }
+    */
+    // LOGIC: It's a brute force approach by using sorting 
+    /*
+    int findDuplicate(vector<int>& nums) 
+    {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                return nums[i];
+            }
+        }
+        return -1;
+    }*/
+};
