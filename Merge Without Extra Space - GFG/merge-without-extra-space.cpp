04909896c40d0672aeece2e5252a1e0bc99d17b5
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-05-22 22:36:38 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -6,11 +6,37 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
+    /*METHOD 2: Better solution: By using constant space
+    LOGIC: Step 1: We will maintain 2 pointer.
+           One at the last index of first array and one at the first index of the second array
+           
+           Step 2: Then we will compare if firstArr[lastElement]>secondArray[firstElement]
+           YES: then swap both the element and decrement the first pointer and increment the second pointer
+           NO: then simply decrement the first pointer and increment the second pointer
+
+           Step 3: When the traversal will be completed all the elements will be at the correct array
+                   but in a random order. Therefore sort both the array
+    
+    DRY RUN: arr1=[1,3,5,7] n=4     arr2=[0,2,6,8,9] m=5
+    STEP 1: Maintain two pointer
+            last=arr1[n-1]   first=arr2[0]
+    STEP 2: while(last >=0 && first<m)
+            {
+                if(arr1[last]>arr2[first])
+                    swap(arr1[last], arr2[first])
+                    last--; first++;
+                else
+                    last--; first++;
+            }
+    STEP 3: sort(arr1) and sort(arr2)
+    */
         void merge(long long arr1[], long long arr2[], int m, int n) 
         {
+            // STEP 1
             int last=m-1;
             int first=0;
     
+            // STEP 2
             while(last>=0 && first<n)
             {
                 if(arr1[last]>arr2[first])
@@ -21,10 +47,13 @@ class Solution{
                 }
                 else
                 {
+                    break;
                     last--;
                     first++;
                 }
             }
+            
+            // STEP 3
             sort(arr1, arr1+m);
             sort(arr2, arr2+n);
         }
