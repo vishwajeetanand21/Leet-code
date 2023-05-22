acd4494a12395bc25187cf8e4ee7a203fc0dedab
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-05-22 22:58:53 +0530

    Time: 26 ms (53.62%) | Memory: 19.2 MB (82.12%) - LeetSync

--- move-zeroes/move-zeroes.cpp
+++ move-zeroes/move-zeroes.cpp
@@ -1,6 +1,12 @@
 class Solution 
 {
 public:
+    /* LOGIC: We will traverse from left to right
+       and we will maintain one pointer at 0th index
+       whenever we get arr[i]!=0
+       YES: then swap(arr[k], arr[i])
+       NO: keep on traversing
+    */
     void moveZeroes(vector<int>& nums) 
     {
         int k=0;//This variable will store all the number which are not equal to zero
@@ -15,7 +21,7 @@ public:
             }
         }
     }
-    /*
+    /* DRY RUN
     0 1 0 3 12
     1 0 0 3 12
     1 3 0 0 12
