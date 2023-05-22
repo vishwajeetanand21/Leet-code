e71204d4a3f933f45f9c6ddda7de439a7f909de9
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-05-22 22:36:52 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -47,7 +47,6 @@ class Solution{
                 }
                 else
                 {
-                    break;
                     last--;
                     first++;
                 }
