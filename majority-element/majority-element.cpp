553dd684593ff9cfe74758f662700a4fb6b305be
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-05-24 15:15:11 +0530

    Time: 27 ms (22.21%) | Memory: 19.5 MB (69.37%) - LeetSync

--- majority-element/majority-element.cpp
+++ majority-element/majority-element.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    int majorityElement(vector<int>&arr)
+    {
+        int n=arr.size();
+        int i, candidate = -1, votes = 0;
+        // Finding majority candidate
+        for (i = 0; i < n; i++) {
+            if (votes == 0) {
+                candidate = arr[i];
+                votes = 1;
+            }
+            else {
+                if (arr[i] == candidate)
+                    votes++;
+                else
+                    votes--;
+            }
+        }
+        int count = 0;
+        // Checking if majority candidate occurs more than n/2
+        // times
+        for (i = 0; i < n; i++) {
+            if (arr[i] == candidate)
+                count++;
+        }
+    
+        if (count > n / 2)
+            return candidate;
+        return -1;
+    }
+    /*METHOD 1: Brute Force Method
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int>myMap;
 
@@ -19,5 +49,5 @@ public:
             }
         }
         return ans;
-    }
+    }*/
 };
