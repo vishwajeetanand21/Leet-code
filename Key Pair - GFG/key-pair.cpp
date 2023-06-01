23c2a12592272e50f3e449ed474c211776d8b1fd
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-01 18:25:35 +0530

    Added solution - LeetHub

--- Key Pair - GFG/key-pair.cpp
+++ Key Pair - GFG/key-pair.cpp
@@ -10,6 +10,18 @@ class Solution{
 public:	
 	// Function to check if array has 2 elements
 	// whose sum is equal to the given value
+	
+	/*
+	LOGIC: Here we will maintain a unordered map to store the element and it's frequency
+	       And we will traverse from left to right in the array
+	       We will store the current element as currElement
+	       and we will calcualte the more needed element by target-currElement
+	       and if that moreNeeded element is already present in our map then we will return TRUe
+	       Otherwise we will store the currentElement in a map
+	       
+	       After traversing the array if we didn't find the sum
+	       Then we will simply return FALSE
+	*/
 	bool hasArrayTwoCandidates(int arr[], int n, int x) {
 	    // code here
 	    unordered_map<int, int>myMap;
