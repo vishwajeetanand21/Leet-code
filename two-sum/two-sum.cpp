8547497e2c0afe98eb9e580637e3ddb4953b34bf
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-01 15:31:43 +0530

    Time: 10 ms (85.68%) | Memory: 10.7 MB (45.29%) - LeetSync

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -4,6 +4,61 @@ class Solution
     vector<int> twoSum(vector<int>& nums, int target) 
     {
         vector<int>answer;
+
+        unordered_map<int, int>myMap;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            int currElement=nums[i];
+            int moreNeeded=target-currElement;
+
+            if(myMap.find(moreNeeded) != myMap.end())
+            {
+                answer.push_back(i);
+                answer.push_back(myMap[moreNeeded]);
+                return answer;
+            }
+            myMap[currElement]=i;
+        }
+        return answer;
+    }
+
+
+    /* This code will not work for this question
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        sort(nums.begin(), nums.end());
+
+        vector<int>ans;
+
+        int left=0, right=nums.size()-1;
+
+        while(left<right)
+        {
+            int sum=nums[left]+nums[right];
+
+            if(sum==target)
+            {
+                ans.push_back(left);
+                ans.push_back(right);
+            }
+            if(sum<target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return ans;        
+    }
+    */
+    
+    /*
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int>answer;
         for(int i=0;i<nums.size();i++)
         {
             for(int j=i+1;j<nums.size();j++)
@@ -16,5 +71,5 @@ class Solution
             }
         }
         return answer;
-    }
+    }*/
 };
