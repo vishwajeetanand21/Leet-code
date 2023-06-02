cdf2c7f44cdca5a5bf3f5f0d02b6708d0fe03618
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-02 21:27:47 +0530

    Time: 9 ms (81.60%) | Memory: 7.6 MB (93.74%) - LeetSync

--- delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -8,11 +8,48 @@
  */
 class Solution {
 public:
+    /*LOGIC: Copy 
+
+                            delNode
+                            ↓
+                        1->2->3->4->5->NULL 
+                                ↑
+                                temp
+          
+    1. we will copy the data of temp to delNode, like this
+    
+                            delNode
+                            ↓
+                        1->2->4->4->5->NULL
+                                ↑
+                                temp
+    
+    2. now update delNode->next=temp->next;
+    
+                            delNode
+                            ↓
+                        1->2->4->5->NULL
+                                ↑
+                        temp->4  
+    
+    3. and make temp->next=NULL
+    
+                            delNode
+                            ↓
+                        1->2->4->5->NULL
+
+                        temp->3->NULL
+    
+    TC: O(1)
+    SC: O(1)
+    */
     void deleteNode(ListNode* node) {
         ListNode*temp=node->next;
 
         node->val=temp->val;
 
         node->next=temp->next;
+
+        temp->next=NULL;
     }
 };
