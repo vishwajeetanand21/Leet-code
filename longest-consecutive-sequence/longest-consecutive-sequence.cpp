4f8f2af6c1a3c37a4274deb906cbff1b103c0617
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-02 22:05:41 +0530

    Time: 321 ms (37.27%) | Memory: 72.5 MB (5.16%) - LeetSync

--- longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,9 +1,54 @@
 class Solution {
 public:
+    int longestConsecutive(vector<int>& arr)
+    {
+        if(arr.size()==0)
+        {
+            return NULL;
+        }
+
+        set<int>mySet;
+        for(int i=0;i<arr.size();i++)
+        {
+            mySet.insert(arr[i]);
+        }
+
+        priority_queue<int, vector<int>, greater<int>>myQueue;
+
+        for(auto  i: mySet)
+        {
+            myQueue.push(i);
+        }
+
+        int ans=1;
+        int temp=1;
+
+        int currentElement=myQueue.top();
+        myQueue.pop();
+
+        while(myQueue.size()!=0)
+        {
+            if(currentElement+1 == myQueue.top())
+            {
+                temp++;
+            }
+            if(currentElement+1 != myQueue.top())
+            {
+                temp=1;
+            }
+
+            currentElement=myQueue.top();
+            myQueue.pop();
+            
+            ans=max(ans, temp);
+        }
+        return ans;
+    }
+
     /*
     LOGIC: BRUTE FORCE 
     We can simply sort the array and run a for loop to find the longest consecutive sequence.
-    */
+    
     int longestConsecutive(vector<int>& arr) 
     {
         int n=arr.size();
@@ -32,4 +77,5 @@ public:
         }
         return ans;
     }
+    */
 };
