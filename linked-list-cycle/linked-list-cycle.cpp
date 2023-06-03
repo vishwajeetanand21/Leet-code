ab22fcd377fb6018bab62cc25c9c80ec811ab456
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-03 23:14:05 +0530

    Time: 24 ms (11.31%) | Memory: 10.6 MB (14.32%) - LeetSync

--- linked-list-cycle/linked-list-cycle.cpp
+++ linked-list-cycle/linked-list-cycle.cpp
@@ -8,29 +8,38 @@
  */
 class Solution {
 public:
+    /*
+    LOGIC: SET METHOD
+        we will traverse the linked list
+        we will maintain a set to store the address of each node
+        and if the address of current node is already present in our set
+        then it means that there is a cycle in the linked list
+*/
     bool hasCycle(ListNode *head) {
+        // set to store the address of each node
         unordered_set<ListNode*>mySet;
 
+        // if there is 0 or 1 node there cannot be any cycle
         if(head==NULL || head->next==NULL)
         {
-            return 0;
+            return false;
         }
 
 
         ListNode*temp=head;
-        bool ans=0;
+        bool ans=false; // answer variable
 
-        while(temp!=NULL)
+        while(temp!=NULL)// traverse the linked list
         {
-            if(mySet.find(temp)!=mySet.end())
+            if(mySet.find(temp)!=mySet.end())// if the address of current node is already present in the set
             {
-                ans=1;
-                break;
+                ans=true;//this means that there is a cycle in the linked list
+                break;//come out of the loop
             }
-            mySet.insert(temp);
-            temp=temp->next;
+            mySet.insert(temp); //add the address of the current node in the set 
+            temp=temp->next;//move the list to the next node
         }
 
-        return ans;
+        return ans;//finally return the answer
     }
 };
