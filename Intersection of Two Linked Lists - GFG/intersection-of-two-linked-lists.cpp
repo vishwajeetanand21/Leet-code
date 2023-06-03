06e064032856200128ffe160fb2133ae32ec957b
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-03 22:34:52 +0530

    Added solution - LeetHub

--- Intersection of Two Linked Lists - GFG/intersection-of-two-linked-lists.cpp
+++ Intersection of Two Linked Lists - GFG/intersection-of-two-linked-lists.cpp
@@ -61,24 +61,53 @@ struct Node
 
 class Solution{
   public:
+     /*
+    WHEN TWO LINKED LIST ARE SAID TO INTERSECT EACH OTHER?
+    -> When the 2 Node address of are same
+     L1:     4→1
+               ↓
+               8→4→5→NULL
+               ↑
+     L2:   5→6→1   
+    
+    Here there are two linked list 
+        L1: 4->1->8->4->5->NULL
+     L2: 5->6->1->8->4->5->NULL
+    
+    Here the address of 8 will be same in both the linked list
+    */
+
+    /*
+    APPROACH: Using Unordered Set
+
+    Step 1: We will traverse the first linked list and store the data of each node in our set
+    Step 2: Then we will traverse the second linked list
+            If the node data is present in the set
+                then we'll create a newNode and keep on adding the common element in the answer list
+            else
+                return NULL 
+    */
     Node* findIntersection(Node* head1, Node* head2)
     {
-        unordered_set<int>mySet;
+        unordered_set<int>mySet;//set to store the data of linked list
         
-        Node*newHead=NULL;
-        Node*newTail=NULL;
+        Node*newHead=NULL;//answer list head
+        Node*newTail=NULL;//answer list tail
         
-        while(head2!=NULL)
+        while(head2!=NULL)//storing the data of second linked list in our set
         {
             mySet.insert(head2->data);
             head2=head2->next;
         }
         
-        while(head1!=NULL)
+        while(head1!=NULL)//traversing the first linked list
         {
-            if(mySet.find(head1->data) != mySet.end())
+            if(mySet.find(head1->data) != mySet.end()) 
+            // if the current data is already present in our set, then we'll add the data in the answer list
             {
-                Node*newNode=new Node(head1->data);
+                Node*newNode=new Node(head1->data);//creating the node of the common data
+                
+                // the below code is for adding the elements in the answer linked list
                 if(newHead==NULL)
                 {
                     newHead=newNode;
@@ -92,6 +121,7 @@ class Solution{
             }
             head1=head1->next;
         }
+        // at the end return the answer linked list
         return newHead;
     }
     
