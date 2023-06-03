ff0b641f7c7ae6eb53455006117eee72764bf538
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-03 23:33:47 +0530

    Added solution - LeetHub

--- Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
@@ -33,13 +33,24 @@ class Solution
 {
     public:
     //Function to sort a linked list of 0s, 1s and 2s.
+    /*LOGIC: Counting sort
+            Count the number of zeros, ones and twos
+            then make a new answer linked list
+            
+            then keep on adding first zeros to the answer list untill it reaches the total number of zeros
+            similarly do it for ones
+            similarly do it for twos
+            
+            finally return the answer linked list
+    */ 
     Node* segregate(Node *head) {
         
-        // Add code here
+        // variables to store the number of 0s, 1s and 2s
         int zeros=0, ones=0, twos=0;
         
-        Node*temp=head;
-        while(temp!=NULL)
+        Node*temp=head; //variable to traverse the given linked list
+        
+        while(temp!=NULL)//traverse the linked list and store the number of 0s, 1s and 2s
         {
             if(temp->data==0)
             {
@@ -56,12 +67,16 @@ class Solution
             temp=temp->next;
         }
         
+        // make an answer linked list
         Node*newHead=NULL;
         Node*newTail=NULL;
         
+        // keep on adding 0 to the answer list untill the count is equal to the total number of zeros
         for(int i=0;i<zeros;i++)
         {
-            Node*newNode=new Node(0);
+            Node*newNode=new Node(0); //make a node with data 0
+            
+            // below code is for adding the data in the answer linked list
             if(newHead==NULL)
             {
                 newHead=newNode;
@@ -74,6 +89,7 @@ class Solution
             }
         }
         
+        // similarly do it for ones
         for(int i=zeros;i<(zeros+ones);i++)
         {
             Node*newNode=new Node(1);
@@ -89,6 +105,7 @@ class Solution
             }
         }
         
+        // similarly do it for twos
         for(int i=(zeros+ones); i<(zeros+ones+twos);i++)
         {
             Node*newNode=new Node(2);
@@ -103,6 +120,8 @@ class Solution
                 newTail=newTail->next;
             }
         }
+        
+        // finally return the answer linked list
         return newHead;
     }
 };
