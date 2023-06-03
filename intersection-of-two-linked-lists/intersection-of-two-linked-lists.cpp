084d206bcf3abc3cdc48a98b1bf2ca1b46ef4354
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-03 22:09:17 +0530

    Time: 56 ms (36.82%) | Memory: 17.1 MB (17.42%) - LeetSync

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,7 +8,56 @@
  */
 class Solution {
 public:
-    // BRUTE FORCE APPROACH: 
+    /*
+    WHEN TWO LINKED LIST ARE SAID TO INTERSECT EACH OTHER?
+    -> When the 2 Node address of are same
+     L1:     4→1
+               ↓
+               8→4→5→NULL
+               ↑
+     L2:   5→6→1   
+    
+    Here there are two linked list 
+        L1: 4->1->8->4->5->NULL
+     L2: 5->6->1->8->4->5->NULL
+    
+    Here the address of 8 will be same in both the linked list
+    */
+
+    /*
+    APPROACH: Using HashMap
+
+    Step 1: We will traverse the first linked list and store the address of each node
+    Step 2: Then we will traverse the second linked list
+            If the node address is present in the map
+                then return that node thereafter of the second list
+            else
+                return NULL 
+    */
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_set<ListNode*>mySet;  //This map will store the address of each node
+
+        // storing the address of each node in our map
+        while(headA!=NULL)
+        {
+            mySet.insert(headA);
+            headA=headA->next;
+        }
+
+        // now traverse the second linked list
+        while(headB!=NULL)
+        {
+            if(mySet.find(headB) != mySet.end())
+            {
+                return headB;
+            }
+
+            headB=headB->next;
+        }
+        return NULL;
+    }
+
+    /* BRUTE FORCE APPROACH: 
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         while(headA != NULL)
         {
@@ -25,5 +74,5 @@ public:
             headA=headA->next;
         }
         return NULL;
-    }
+    }*/
 };
