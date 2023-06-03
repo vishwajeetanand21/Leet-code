0072ce7a8906dfa83a36c0107e6b741bf3a9ce70
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-03 22:56:08 +0530

    Added solution - LeetHub

--- Intersection of two sorted Linked lists - GFG/intersection-of-two-sorted-linked-lists.cpp
+++ Intersection of two sorted Linked lists - GFG/intersection-of-two-sorted-linked-lists.cpp
@@ -82,18 +82,36 @@ struct Node
 
 */
 
+/*
+The logic is simple
+L1: 1->2->3->4->6->NULL
+L2: 2->4->6->8->NULL
+
+Both the linked list are sorted
+
+Step 1: we will create an answer linked list to store the common elements in both the list
+Step 2: We will traverse both the list
+            if the head1->data == head2->data :This means it's a common element
+                create a newNode and add to the answer linked list
+            if the head1->data < head2->data 
+                move the head1 to the next node
+            if the head1->data > head2->data
+                move the second linked list to the next node
+Step 3: By this all the common elements will be appended to the answer linked list
+*/
 Node* findIntersection(Node* head1, Node* head2)
 {
-    // Your Code Here
+    // answer linked list
     Node*newHead=NULL;
     Node*newTail=NULL;
     
-    while(head1!=NULL && head2!=NULL)
+    while(head1!=NULL && head2!=NULL) //traverse both the linked list simultaneously
     {
-        if(head1->data == head2->data)
+        if(head1->data == head2->data)//it's a common element
         {
-            Node*newNode=new Node(head1->data);
+            Node*newNode=new Node(head1->data);//create a newNode and append it to the answer linked list
             
+            // below code is for appending the newNode to the answer linked list
             if(newHead==NULL)
             {
                 newHead=newNode;
@@ -108,14 +126,17 @@ Node* findIntersection(Node* head1, Node* head2)
             head2=head2->next;
         }
         
+        // this means data of first list is less than the data of the second list
         else if(head1->data < head2->data)
         {
-            head1=head1->next;
+            head1=head1->next; //move the first list to the next node
         }
+        
+        //this means data of the second list is less than the data of the first list 
         else if(head1->data > head2->data)
         {
-            head2=head2->next;
+            head2=head2->next;//move the second list to the next node
         }
     }
-    return newHead;
+    return newHead;//finally return the answer list
 }
