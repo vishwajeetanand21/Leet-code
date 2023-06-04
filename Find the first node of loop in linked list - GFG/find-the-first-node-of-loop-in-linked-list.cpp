f43c2c34b2a6abefa3711a62e0458ce9a0fee6e2
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-04 22:50:23 +0530

    Added solution - LeetHub

--- Find the first node of loop in linked list - GFG/find-the-first-node-of-loop-in-linked-list.cpp
+++ Find the first node of loop in linked list - GFG/find-the-first-node-of-loop-in-linked-list.cpp
@@ -43,9 +43,55 @@ void loopHere(Node* head, Node* tail, int position)
 class Solution
 {
     public:
+    /*
+    Approach: USING FAST AND SLOW POINTER
+
+The following steps are required:
+    -> Initially take two pointers, fast and slow. 
+    -> The fast pointer takes two steps ahead while the slow pointer will take a single step.
+    -> We know that if a cycle exists, fast and slow pointers will collide.
+    -> If the cycle does not exist, the fast pointer will move to NULL
+    -> Else, when both slow and fast pointer collides, it detects a cycle exists.
+    -> Take another pointer, say entry. Point to the very first of the linked list.
+    -> Move the slow and the entry pointer ahead by single steps until they collide. 
+    -> Once they collide we get the starting node of the linked list.
+
+    FOR REFERENCE: https://takeuforward.org/data-structure/starting-point-of-loop-in-a-linked-list/
+*/
+    int findFirstNode(Node* head)
+    {
+        if(head==NULL || head->next==NULL)
+        {
+            return head->data;
+        }
+        
+        Node*slow=head;
+        Node*fast=head;
+        Node*entry=head;
+        
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            
+            if(slow==fast)
+            {
+                while(slow!=entry)
+                {
+                    slow=slow->next;
+                    entry=entry->next;
+                }
+                return slow->data;
+            }
+        }
+        return -1;
+    }
+    
+    
+    
      //Function to find first node if the linked list has a loop.
-    // BRUTE FORCE SOLUTION: USING SET TO STORE THE ADDRESS OF EACH NODE
-    // AND IF THE ADDRESS IS ALREADY PRESENT IN THE SET, IT MEANS THE CYCLE START FROM THAT NODE
+    /* BRUTE FORCE SOLUTION: USING SET TO STORE THE ADDRESS OF EACH NODE
+    AND IF THE ADDRESS IS ALREADY PRESENT IN THE SET, IT MEANS THE CYCLE START FROM THAT NODE
     int findFirstNode(Node* head)
     {
         // your code here
@@ -63,7 +109,7 @@ class Solution
             temp=temp->next;
         }
         return -1;
-    }
+    }*/
 };
 
 //{ Driver Code Starts.
