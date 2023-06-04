d774059914474bef815894d63d99f503b51bd594
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-04 22:43:34 +0530

    Time: 8 ms (80.82%) | Memory: 7.7 MB (54.29%) - LeetSync

--- linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -8,6 +8,49 @@
  */
 class Solution {
 public:
+    /*
+    Approach:
+
+The following steps are required:
+
+    -> Initially take two pointers, fast and slow. 
+    -> The fast pointer takes two steps ahead while the slow pointer will take a single step.
+    -> We know that if a cycle exists, fast and slow pointers will collide.
+    -> If the cycle does not exist, the fast pointer will move to NULL
+    -> Else, when both slow and fast pointer collides, it detects a cycle exists.
+    -> Take another pointer, say entry. Point to the very first of the linked list.
+    -> Move the slow and the entry pointer ahead by single steps until they collide. 
+    -> Once they collide we get the starting node of the linked list.
+    */
+    ListNode *detectCycle(ListNode *head) {
+        if(head==NULL || head->next==NULL)
+        {
+            return NULL;
+        }
+
+        ListNode*slow=head;
+        ListNode*fast=head;
+        ListNode*entry=head;
+
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+            {
+                while(slow!=entry)
+                {
+                    entry=entry->next;
+                    slow=slow->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+
+    /* BRUTE FORCE SOLUTION: USING SET TO STORE THE ADDRESS OF EACH NODE
+     AND IF THE ADDRESS IS ALREADY PRESENT IN THE SET, IT MEANS THE CYCLE START FROM THAT NODE
     ListNode *detectCycle(ListNode *head) {
         unordered_set<ListNode*>mySet;
 
@@ -22,5 +65,5 @@ public:
             temp=temp->next;
         }
         return NULL;
-    }
+    }*/
 };
