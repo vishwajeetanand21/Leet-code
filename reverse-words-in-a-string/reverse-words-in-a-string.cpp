d68ec7a7b9e988e13510814fc5de02fb1375aaa3
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-19 22:22:56 +0530

    Time: 19 ms (10.65%) | Memory: 46.7 MB (8.75%) - LeetSync

--- reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,44 +2,21 @@ class Solution {
 public:
     string reverseWords(string s) 
     {
-        string ans="";
-
-        string temp="";
-
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]!=' ')
-            {
-                temp.push_back(s[i]);
-            }
-            else if(s[i]==' ')
-            {
-                reverse(temp.begin(), temp.end());
-                ans=ans+temp+" ";
-                temp="";
-            }
-        }
-        reverse(temp.begin(), temp.end());
-        ans=ans+temp;
-
-        reverse(ans.begin(), ans.end());
-
-        
         // handling the extra space in the sentence
         string answer="";
 
-        for(int i=0;i<ans.size();i++)
+        for(int i=0;i<s.size();i++)
         {
-            if(ans[i]==' ')
+            if(s[i]==' ')
             {
 
             }
-            else if(ans[i]!=' ')
+            else if(s[i]!=' ')
             {
                 int j=i;
-                while(ans[j]!=' ' && i<ans.size() && j<ans.size())
+                while(s[j]!=' ' && i<s.size() && j<s.size())
                 {
-                    answer.push_back(ans[j]);
+                    answer.push_back(s[j]);
                     j++;
                 }
                 answer.push_back(' ');
@@ -47,7 +24,37 @@ public:
             }
         }
         answer.pop_back();
+        //till now all the extra space have been remove from our string
+        // now just we have to reverse each word and then reverse entire sentence
 
-        return answer;
+
+
+        string ans=""; //variable to store the answer
+
+        string temp=""; //variable to reverse each word
+
+        // traversing from starting index to the end index
+        for(int i=0;i<answer.size();i++)
+        {
+            if(answer[i]!=' ') //if there is no space then add the character to the temp variable
+            {
+                temp.push_back(answer[i]);
+            }
+            else if(answer[i]==' ')//if we found a space 
+            {
+                reverse(temp.begin(), temp.end());//then we have to reverse that particular word
+                ans=ans+temp+" ";//append that word to the answer
+                temp=""; //update the temp variable to reverse the next upcoming word
+            }
+        }
+        //handle the last word seprately
+        reverse(temp.begin(), temp.end());
+        ans=ans+temp;
+
+        // reverse the entire sentence once again
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+        
     }
 };
