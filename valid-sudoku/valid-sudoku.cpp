295b076f34728d005822a5305590095909f019fa
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-30 14:50:33 +0530

    Time: 18 ms (88.34%) | Memory: 17.8 MB (99.94%) - LeetSync

--- valid-sudoku/valid-sudoku.cpp
+++ valid-sudoku/valid-sudoku.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
+/*
+LOGIC: To check whether it's a valid sudoku or not
+       STEP 1: Traverse in all the cells of sudoku
+       STEP 2: Whenever there is a numerical digit store it in a variable 'digit'
+       STEP 3: Now call a isSafe() function and pass the array, current row and current column
+            (i) Replace the numbrical character with a '.'
+            (ii) Then find across current row whether this digit is present or not. Do the same for current column
+                 and grid 
+            (iii) If the digit is present return false
+            (iv) After traversing in the current row and current column and current grid if the digit is not
+                 present return true
+            (v) Then finally replace the '.' with the original character     
+*/
     bool isSafe(vector<vector<char>>&arr, int currRow, int currCol)
     {
-        char digit = arr[currRow][currCol];
+        char digit = arr[currRow][currCol];//storing the current digit
 
-        arr[currRow][currCol]='.';
+        arr[currRow][currCol]='.';//and replacing it with a '.'
 
+        // finding in current row
         for(int i=0;i<9;i++)
         {
             if(arr[currRow][i]==digit)
@@ -14,6 +28,7 @@ public:
             }
         }
 
+        // finding in current column
         for(int i=0;i<9;i++)
         {
             if(arr[i][currCol]==digit)
@@ -22,6 +37,7 @@ public:
             }
         }
 
+        // finding in current grid
         int x=(currRow/3)*3;
         int y=(currCol/3)*3;
 
@@ -35,8 +51,11 @@ public:
                 }
             }
         }
+
+        // replacing the '.' with the original digit
         arr[currRow][currCol]=digit;
 
+        // finally returning true
         return true;
     }
     bool solve(vector<vector<char>>&arr)
@@ -46,12 +65,16 @@ public:
             for(int currCol=0; currCol<9;currCol++)
             {
                 char digit=arr[currRow][currCol];
-                if(digit!='.' && !isSafe(arr,currRow, currCol))
+                // the digit should any numerical value it should not be any space '.'
+                // and the isSafe() function should also return false
+                // then only we can say that the given sudoku is not valid
+                if(digit!='.' && isSafe(arr,currRow, currCol)==false)
                 {
                     return false;
                 }
             }
         }
+        // otherwise it's a valid sudoku
         return true;
     }
     bool isValidSudoku(vector<vector<char>>& arr)
@@ -65,44 +88,4 @@ public:
             return false;
         }
     } 
-    /*
-    // To check if it is valid,
-    // Firstly, replace the character with a '.' so that we can check even after this it is already present or not .. 
-    //  - Check for the entire row if it already has the same character twice? Return false if it does
-    //  - Check for the entire col if it already has the same character twice? Return false if it does
-    //  - Check for the 3x3 box to see it already has it? Return false if it does
-    // Replace the incorrect '.' back to the character c
-    
-    // NOTE: Given a row, col, if we need to find the starting i,j of that 3x3 box, then we do row/3*3 and col/3*3
-    
-    bool isValid(vector<vector<char>>& board, int row, int col){
-        int c = board[row][col];
-        board[row][col] = '.';
-        for(int i=0;i<9;i++)
-            if(board[row][i] == c) return false;
-        for(int i=0;i<9;i++)
-            if(board[i][col] == c) return false;
-        for(int i=0;i<3;i++)
-            for(int j=0;j<3;j++)
-                if(board[row/3*3 + i][col/3*3 + j] == c)
-                    return false;
-        
-        board[row][col] = c;
-        return true;
-    }
-    bool isValidSudoku(vector<vector<char>>& board) 
-    {
-        // Go for all the rows & columns looking for an invalid number
-        // If you find an invalid entry, return false
-        // If you don not find an invalid entry till the end, it has to be good, return true.
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(board[i][j]!='.' && !isValid(board,i,j)){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    */
 };
