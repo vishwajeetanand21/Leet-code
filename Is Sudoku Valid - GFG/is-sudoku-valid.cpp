f3462e8b52dcb6bd0a8cb0d90398ee9354a0f130
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-06-30 14:59:00 +0530

    Added solution - LeetHub

--- Is Sudoku Valid - GFG/is-sudoku-valid.cpp
+++ Is Sudoku Valid - GFG/is-sudoku-valid.cpp
@@ -9,11 +9,24 @@ using namespace std;
 
 class Solution{
 public:
+/*
+LOGIC: To check whether it's a valid sudoku or not
+       STEP 1: Traverse in all the cells of sudoku
+       STEP 2: Whenever there is a numerical digit store it in a variable 'digit'
+       STEP 3: Now call a isSafe() function and pass the array, current row and current column
+            (i) Replace the numbrical character with a '.'
+            (ii) Then find across current row whether this digit is present or not. Do the same for current column
+                 and grid 
+            (iii) If the digit is present return false
+            (iv) After traversing in the current row and current column and current grid if the digit is not
+                 present return true
+            (v) Then finally replace the '.' with the original character     
+*/
     bool isSafe(vector<vector<int>>arr, int currRow, int currCol)
     {
-        int digit=arr[currRow][currCol];
+        int digit=arr[currRow][currCol]; //storing the current digit
         
-        arr[currRow][currCol]=0;
+        arr[currRow][currCol]=0;//and replacing it with a 0
         
         // current row
         for(int i=0;i<9;i++)
@@ -47,9 +60,9 @@ public:
                 }
             }
         }
-        
+        // replacing the 0 with the original digit
         arr[currRow][currCol]=digit;
-        
+        // finally returning true
         return true;
     }
     bool solve(vector<vector<int>>arr)
@@ -59,13 +72,16 @@ public:
             for(int currCol=0;currCol<9;currCol++)
             {
                 int digit=arr[currRow][currCol];
-                
+                // the digit should any numerical value it should not be any 0
+                // and the isSafe() function should also return false
+                // then only we can say that the given sudoku is not valid
                 if(digit!=0 && isSafe(arr,currRow, currCol)==false)
                 {
                     return false;
                 }
             }
         }
+        // otherwise it's a valid sudoku
         return true;
     }
     int isValid(vector<vector<int>> arr){
