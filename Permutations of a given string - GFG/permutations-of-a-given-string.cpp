84ffeb800cb449f63c292803fb6e0f75d01ea852
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-01 18:49:01 +0530

    Added solution - LeetHub

---
+++ Permutations of a given string - GFG/permutations-of-a-given-string.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+        void recurPermute(int index, string&nums, vector<string>&ans) 
+        {
+            if (index == nums.size()) 
+            {
+                ans.push_back(nums);
+                return;
+            }
+            
+            for (int i = index; i < nums.size(); i++) 
+            {
+                swap(nums[index], nums[i]);
+                recurPermute(index + 1, nums, ans);
+                swap(nums[index], nums[i]);
+            }
+        }
+		vector<string>find_permutation(string nums)
+		{
+		    // Code here there
+		    vector <string> ans;
+            recurPermute(0, nums, ans);
+            
+            set<string>uniqueAns;
+            for(int i=0;i<ans.size();i++)
+            {
+                uniqueAns.insert(ans[i]);
+            }
+            
+            vector<string>finalAns;
+            for(auto &i: uniqueAns)
+            {
+                finalAns.push_back(i);
+            }
+            return finalAns;
+		}
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+	    string S;
+	    cin >> S;
+	    Solution ob;
+	    vector<string> ans = ob.find_permutation(S);
+	    sort(ans.begin(),ans.end());
+	    for(auto i: ans)
+	    {
+	    	cout<<i<<" ";
+	    }
+	    cout<<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
