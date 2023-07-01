111a2ca5a791ba5f9509a316a4ae1f1c07b0bb15
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-01 22:01:26 +0530

    Time: 7 ms (27.43%) | Memory: 7.5 MB (84.68%) - LeetSync

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     void recurPermute(int index, vector < int > & nums, vector < vector < int >> & ans) {
-      if (index == nums.size()) {
-        ans.push_back(nums);
-        return;
-      }
-      for (int i = index; i < nums.size(); i++) {
-        swap(nums[index], nums[i]);
-        recurPermute(index + 1, nums, ans);
-        swap(nums[index], nums[i]);
-      }
+        if (index == nums.size()) //whenever the index reaches n it means we got one solution
+        {
+            ans.push_back(nums);//add the solution to the answer vector
+            return; 
+        }
+        for (int i = index; i < nums.size(); i++) //traverse from left to right
+        {
+            swap(nums[index], nums[i]); //and swap the index element and ith element
+            recurPermute(index + 1, nums, ans); //call the recursive function again
+            swap(nums[index], nums[i]); //backtracking line
+        }
     }
     vector<vector<int>> permute(vector<int>& nums) 
     {
-      vector < vector < int >> ans;
-      recurPermute(0, nums, ans);
-      return ans;
+      vector < vector < int >> ans;//vector to store all the possible permutations
+      recurPermute(0, nums, ans); //calling the recursive function
+      return ans; //finally returning the answer
     }
 };
