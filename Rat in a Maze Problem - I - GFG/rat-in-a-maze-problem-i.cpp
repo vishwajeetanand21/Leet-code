a628c5c4725cb3a02479cc02b724390f875d0d3d
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-07 22:51:29 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -10,61 +10,74 @@ using namespace std;
 
 class Solution{
     public:
-    
-    void findPathHelper(int currRow, int currCol, vector<vector<int>>&mat, int n, vector<string>&ans, string move, vector<vector<int>>&visited)
+    bool isSafe(int currRow, int currCol, vector<vector<int>>&visited, vector<vector<int>>&matrix, int n)
+    {
+        if(currRow<n && currRow>=0 && currCol<n && currCol>=0)
+        {
+            if(visited[currRow][currCol]==0)
+            {
+                if(matrix[currRow][currCol]==1)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+        return false;
+    }
+    void findPathHelper(int currRow, int currCol, vector<vector<int>>&matrix, int n, vector<string>&answer, string move, vector<vector<int>>&visited)
     {
         if(currRow==n-1 && currCol==n-1)
         {
-            ans.push_back(move);
+            answer.push_back(move);
             return ;
         }
         
-        // downward
-        if(currRow+1<n    &&   visited[currRow+1][currCol]==0     &&      mat[currRow+1][currCol]==1)
+        // down
+        if(isSafe(currRow+1, currCol, visited, matrix, n)==true)
         {
             visited[currRow][currCol]=1;
-            findPathHelper(currRow+1, currCol, mat, n, ans, move+"D", visited);
-            visited[currRow][currCol]=0;//backtracking line 
+            findPathHelper(currRow+1, currCol, matrix, n, answer, move+"D", visited);
+            visited[currRow][currCol]=0;
         }
         
         // left
-        if(currCol-1>=0   &&   visited[currRow][currCol-1]==0     &&      mat[currRow][currCol-1]==1)
+        if(isSafe(currRow, currCol-1, visited, matrix, n)==true)
         {
             visited[currRow][currCol]=1;
-            findPathHelper(currRow, currCol-1, mat, n, ans, move+"L", visited);
+            findPathHelper(currRow, currCol-1, matrix, n, answer, move+"L", visited);
             visited[currRow][currCol]=0;
         }
         
         // right
-        if(currCol+1<n    &&   visited[currRow][currCol+1]==0     &&      mat[currRow][currCol+1]==1)
+        if(isSafe(currRow, currCol+1, visited, matrix, n)==true)
         {
             visited[currRow][currCol]=1;
-            findPathHelper(currRow, currCol+1, mat, n, ans, move+"R", visited);
+            findPathHelper(currRow, currCol+1, matrix, n, answer, move+"R", visited);
             visited[currRow][currCol]=0;
         }
         
-        // upward
-        if(currRow-1>=0   &&   visited[currRow-1][currCol]==0     &&      mat[currRow-1][currCol]==1)
+        // up
+        if(isSafe(currRow-1, currCol, visited, matrix, n)==true)
         {
             visited[currRow][currCol]=1;
-            findPathHelper(currRow-1, currCol, mat, n, ans, move+"U", visited);
+            findPathHelper(currRow-1, currCol, matrix, n, answer, move+"U", visited);
             visited[currRow][currCol]=0;
         }
-    }  
-    vector<string> findPath(vector<vector<int>> &mat, int n) 
+    }
+    vector<string> findPath(vector<vector<int>> &matrix, int n) 
     {
-        // Your code goes here
-            
-        // declaring a vector to store all the possible paths
-        vector<string>ans;
-        vector<vector<int>>visited(n, vector<int>(n,0));
+        vector<string>answer;
         
-        if (mat[0][0] == 1) 
+        if(matrix[0][0]==1)
         {
-            findPathHelper(0, 0, mat, n, ans, "", visited);
+            vector<vector<int>>visited(n, vector<int>(n, 0));
+            
+            findPathHelper(0, 0, matrix, n, answer, "", visited);
         }
-       
-        return ans;
+        
+        return answer;
     }
 };
 
