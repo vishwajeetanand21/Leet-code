f8bb1edd527bce64719bb115c672df99e0141947
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-24 01:39:36 +0530

    Added solution - LeetHub

--- Number of 1 Bits - GFG/number-of-1-bits.cpp
+++ Number of 1 Bits - GFG/number-of-1-bits.cpp
@@ -5,6 +5,29 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+    /*
+    LOGIC: n= 13
+    
+         n= 1 1 0 1 
+       n&1= 1 1 0 1  count=1
+       n=n>>1
+       
+         n= 0 1 1 0
+       n&1= 0 0 0 1  count=1
+       n=n>>1
+       
+         n= 0 0 1 1
+       n&1= 0 0 0 1  count=2
+       n=n>>1
+       
+         n= 0 0 0 1
+       n&1= 0 0 0 1 count=3
+       n=n>>1
+       
+         n= 0 0 0 0
+         return count
+        
+    */
     int setBits(int n) 
     {
         // Write Your Code here
