94502b1433a6e2066fa451b3b6d51ed5237ac1e6
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-23 21:49:42 +0530

    Added solution - LeetHub

--- Set kth bit - GFG/set-kth-bit.cpp
+++ Set kth bit - GFG/set-kth-bit.cpp
@@ -6,13 +6,32 @@ using namespace std;
 class Solution
 {
 public:
+    /*
+    LOGIC: Making a mask and performing OR operation
+    
+    n=10    k=2
+    
+    n= 1 (0) 1 0    We have to set the 2nd index bit from 0 to 1
+    
+    create a mask
+    mask= 1<<k
+    mask= 0 1 0 0
+    
+    n OR mask
+    1  0 1 0    |   0  1  0 0
+    1 (0) 1 0   |   0 (1) 0 0
+    1 1 1 0
+    14
+    
+    
+    */
     int setKthBit(int n, int k)
     {
         // Write Your Code here
         
-        int mask=1<<k;
+        int mask=1<<k; // make a mask, only the kth bit will be 1 rest all the other bit will be 0
         
-        return n|mask;
+        return n|mask; //perform the OR operation of n and mask
     }
     
 };
