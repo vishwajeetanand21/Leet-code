d9f3955800056c1b2bd252033ba7cfd12d2f010f
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-23 18:53:04 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (54.03%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int xorOperation(int n, int start) 
     {
+        int ans=0;
+
         vector<int>arr;
         
         for(int i=0; i<n;i++)
         {
-            int temp=start+2*i;
-            arr.push_back(temp);
+            // int temp=start+2*i;
+            // arr.push_back(temp);
+            ans^=(start+2*i);
         }
 
-        int ans=arr[0];
+        /*int ans=arr[0];
         for(int i=1;i<arr.size();i++)
         {
             ans^=arr[i];
-        }
+        }*/
 
         return ans;
     }
