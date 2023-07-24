e747746d7b23bcba9e9975f85a866383ea11d608
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-24 19:29:32 +0530

    Time: 40 ms (57.13%) | Memory: 26.8 MB (29.80%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -2,6 +2,52 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& arr) 
     {
+        int breakPoint;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]>=0)
+            {
+                breakPoint=i;
+                break;
+            }
+        }
+
+        for(int i=0;i<arr.size();i++)
+        {
+            arr[i]=arr[i]*arr[i];
+        }
+
+        vector<int>ans;
+        int i=breakPoint-1, j=breakPoint;
+        while(i>=0 && j<arr.size())
+        {
+            if(arr[i]<arr[j])
+            {
+                ans.push_back(arr[i]);
+                i--;
+            }
+            else
+            {
+                ans.push_back(arr[j]);
+                j++;
+            }
+        }
+        while(i>=0)
+        {
+            ans.push_back(arr[i]);
+            i--;
+        }
+        while(j<arr.size())
+        {
+            ans.push_back(arr[j]);
+            j++;
+        }
+
+        return ans;
+    }
+    /* BRUTE FORCE APPROACH
+    vector<int> sortedSquares(vector<int>& arr) 
+    {
         for(int i=0;i<arr.size();i++)
         {
             arr[i]= arr[i]*arr[i];
@@ -11,4 +57,5 @@ public:
 
         return arr;
     }
+    */
 };
