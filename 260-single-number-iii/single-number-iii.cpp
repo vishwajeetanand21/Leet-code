bc4263c0714c94b7fdc6d8889a32441568003b22
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-24 23:42:05 +0530

    Time: 10 ms (59.83%) | Memory: 10 MB (41.50%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,9 +1,42 @@
 class Solution {
 public:
+    vector<int> singleNumber(vector<int>& A) 
+    {
+        int n=A.size();
+        int mask=1;
+        int Xor=0;
+        for(int i=0;i<n;i++)
+        {
+            Xor^=A[i];
+            
+        }
+        
+        while((Xor &mask)==0)
+        {
+            mask<<=1;
+        }
+        
+        int j=0,k=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if((mask & A[i])==0) j^=A[i];
+            else k^=A[i];
+            
+        }
+        cout<<mask;
+        
+        vector<int>ans;
+        ans.push_back(j);
+        ans.push_back(k);
+        return ans;
+    }
+
+    /* BRUTE FORCE METHOD: USING MAP
     vector<int> singleNumber(vector<int>& arr) 
     {
         int n=arr.size();
-        
+
         unordered_map<int, int>myMap;
     
         for(int i=0;i<n;i++)
@@ -19,7 +52,8 @@ public:
                 ans.push_back(i.first);
             }
         }
-        
+    
         return ans;    
     }
+    */
 };
