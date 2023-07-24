69e929fff9010479705a9fff859472664acbae29
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-24 19:18:50 +0530

    Time: 53 ms (14.91%) | Memory: 25.8 MB (92.38%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& arr) 
     {
-        // map<int, int>myMap;
-
         for(int i=0;i<arr.size();i++)
         {
             arr[i]= arr[i]*arr[i];
