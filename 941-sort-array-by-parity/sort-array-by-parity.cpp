302574f1946a7cbabf882b18b052160e80c4ebdd
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-24 20:45:26 +0530

    Time: 11 ms (70.18%) | Memory: 16.1 MB (94.28%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -3,8 +3,11 @@ public:
     vector<int> sortArrayByParity(vector<int>& arr) 
     {
         for (int i = 0, j = 0; j < arr.size(); j++)
-            if ( (arr[j]&1) == 0) 
-                swap(arr[i++], arr[j]);
+            if ( (arr[j]&1) == 0) //even element
+            {
+                swap(arr[i], arr[j]);
+                i++;
+            }
         return arr;
     }
 };
