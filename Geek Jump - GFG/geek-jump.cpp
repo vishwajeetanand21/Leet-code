4e4b3f163991a5f50e5caf76fdd8462e8fb73204
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-27 01:19:47 +0530

    Added solution - LeetHub

--- Geek Jump - GFG/geek-jump.cpp
+++ Geek Jump - GFG/geek-jump.cpp
@@ -6,99 +6,39 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    // using variables: space optimization
-    int usingVariables(vector<int>&heights, int index)
+    int usingMemoizationHelper(int index, vector<int>&height, vector<int>&dp)
     {
+        // base case
         if(index==0)
         {
             return 0;
         }
-    
-        int a=0, b=0, c;
-    
-        for(int index=1; index<heights.size(); index++)
-        {
-            int jumpTwo=INT_MAX;
-    
-            int jumpOne=b+ abs(heights[index] - heights[index-1]);
-    
-            if(index>1)
-            {
-                jumpTwo=a+ abs(heights[index] - heights[index-2]);
-            }
-    
-            c=min(jumpOne, jumpTwo);
-    
-            a=b;
-            b=c;
-        }
-        return c;
-    }  
-      
-    // using tabulation
-    int usingTabluation(vector<int>&heights, int index)
-    {
-        int n=heights.size();
-        int dp[n];
         
-        dp[0]=0;
-    
-        for(int index=1;index<n;index++)
-        {
-            int jumpTwo=INT_MAX;
-    
-            int jumpOne=dp[index-1] + abs(heights[index] - heights[index-1]);
-    
-            if(index>1)
-            {
-                jumpTwo=dp[index-2] + abs(heights[index] - heights[index-2]);
-            }
-    
-            dp[index]=min(jumpOne, jumpTwo);
-        }
-    
-        return dp[n-1];
-    }
-  
-    // using memoization
-    int usingMemoizationHelper(int index, vector<int>&heights, int dp[])
-    {
-        if(index==0)
-        {
-            return 0;
-        }
-    
         if(dp[index]!=-1)
         {
             return dp[index];
         }
-    
+        
         int jumpTwo=INT_MAX;
-    
-        int jumpOne=usingMemoizationHelper(index-1, heights, dp) + abs(heights[index]-heights[index-1]);
-    
+        
+        int jumpOne=usingMemoizationHelper(index-1, height,dp)+abs(height[index]-height[index-1]);
+        
         if(index>1)
         {
-            jumpTwo=usingMemoizationHelper(index-2, heights, dp) + abs(heights[index]-heights[index-2]);
+            jumpTwo=usingMemoizationHelper(index-2, height,dp)+abs(height[index]-height[index-2]);
         }
-    
+        
         return dp[index]=min(jumpOne, jumpTwo);
     }
-    int usingMemoization(vector<int>&heights, int index)
+    int usingMemoization(int index, vector<int>&height)
     {
-        int n=heights.size();
-        int dp[n];
-        for(int i=0;i<n;i++)
-        {
-            dp[i]=-1;
-        }
-    
-        return usingMemoizationHelper(index, heights, dp);
+        vector<int>dp(index+1,-1);
+        return usingMemoizationHelper(index, height,dp);
     }
     
-    // using recursion
-    int usingRecursion(vector<int>&heights, int index)
+    int usingRecursion(int index, vector<int>&height)
     {
+        // base case
         if(index==0)
         {
             return 0;
@@ -106,21 +46,20 @@ class Solution {
         
         int jumpTwo=INT_MAX;
         
-        int jumpOne=usingRecursion(heights, index-1) + abs(heights[index]-heights[index-1]);
+        int jumpOne=usingRecursion(index-1, height)+abs(height[index]-height[index-1]);
         
-        if(index>1)
+        if(index>1)//if the index will be greater than 1, then only we can make two jump
+        // otherwise if the index is 1 and if we do two left jumps then we will go out of the boundary
         {
-            jumpTwo=usingRecursion(heights, index-2) + abs(heights[index]-heights[index-2]);
+            jumpTwo=usingRecursion(index-2,height)+abs(height[index]-height[index-2]);
         }
-        
         return min(jumpOne, jumpTwo);
     }
-    int minimumEnergy(vector<int>& heights, int n) {
+    int minimumEnergy(vector<int>& height, int n) 
+    {
         // Code here
-        // return usingRecursion(heights, n-1);
-        // return usingMemoization(heights, n-1);
-        // return usingTabluation(heights,n-1);
-        return usingVariables(heights, n-1);
+        // return usingRecursion(n-1, height);
+        return usingMemoization(n-1, height);
     }
 };
 
