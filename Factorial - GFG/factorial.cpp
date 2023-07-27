af84da27759b4e39345fb9a784fc1873af83a46b
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-27 22:40:17 +0530

    Added solution - LeetHub

--- Factorial - GFG/factorial.cpp
+++ Factorial - GFG/factorial.cpp
@@ -5,20 +5,54 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 public:
-    long long int factorial(int n){
-        //code here
-            // base case
-        if(n==0)
+    // using variables
+    long long int usingVariables(int n)
+    {
+        if(n<=1)
         {
             return 1;
         }
         
-        long long ans=1;
-        for(int i=1;i<=n;i++)
+        long long int a=1; //for 0
+        long long int b=1; //for 1
+        long long int c;
+        
+        for(int i=2;i<=n;i++)
         {
-            ans*=i;
+            c=i*b;
+            b=c;
         }
-        return ans;
+        return c;
+    }
+    
+    // using tabulation
+    long long int usingTabulation(int n)
+    {
+        vector<int>dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2; i<=n;i++)
+        {
+            dp[i]=i*dp[i-1];
+        }
+        return dp[n];
+    }
+    
+    // using recursion
+    long long int usingRecursion(int n)
+    {
+        if(n==1)
+        {
+            return 1;
+        }
+        return n*usingRecursion(n-1);
+    }
+    long long int factorial(int n){
+        //code here
+        // return usingRecursion(n);
+        // return usingTabulation(n);
+        return usingVariables(n);
     }
 };
 
