110ae37c73b02db758301c573e961bb65e00f9e6
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-27 23:55:02 +0530

    Added solution - LeetHub

--- Number of paths - GFG/number-of-paths.cpp
+++ Number of paths - GFG/number-of-paths.cpp
@@ -5,6 +5,34 @@ using namespace std;
 
 // } Driver Code Ends
 
+// using tabulation
+int usingTabulation(int a, int b)
+{
+    vector<vector<int>>dp(a, vector<int>(b,-1));
+    
+    for(int i=0;i<a;i++)
+    {
+        for(int j=0;j<b;j++)
+        {
+            if(i==0 && j==0)//base case
+            {
+                dp[i][j]=1;
+            }
+            else
+            {
+                int up=0, left=0;
+                if(i>0)
+                    up=dp[i-1][j];
+                if(j>0)
+                    left=dp[i][j-1];
+                    
+                dp[i][j]=up+left;
+            }
+        }
+    }
+    return dp[a-1][b-1];
+}
+
 // using memoization
 int usingMemoizationHelper(int i, int j, vector<vector<int>>& dp) 
 {
@@ -56,7 +84,8 @@ long long  numberOfPaths(int a, int b)
 {
     // Code Here
     // return usingRecursion(a-1,b-1);
-    return usingMemoization(a, b);
+    // return usingMemoization(a, b);
+    return usingTabulation(a,b);
 }
 
 //{ Driver Code Starts.
