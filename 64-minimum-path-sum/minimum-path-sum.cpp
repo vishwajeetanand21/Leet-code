9946d60c51ae413ae5caf90caca3ef667dffc56a
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-28 01:11:04 +0530

    Time: 7 ms (93.33%) | Memory: 10 MB (43.50%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,5 +1,40 @@
 class Solution {
 public:
+    // using tabulation
+    int usingTabulation(int n, int m, vector<vector<int>>&grid)
+    {
+        vector<vector<int>>dp(n, vector<int>(m, -1));
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0 && j==0)
+                {
+                    dp[i][j]=grid[i][j];
+                }
+                else
+                {
+                    int up=grid[i][j];
+                    int left=grid[i][j];
+                    
+                    if(i>0)
+                        up+=dp[i-1][j];
+                    else
+                        up+=1e9;
+                    
+                    if(j>0)
+                        left+=dp[i][j-1];
+                    else
+                        left+=1e9;
+                    
+                    dp[i][j]=min(up,left);
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+
     // using memoization
     int usingMemoizationHelper(int i, int j, vector<vector<int>>&grid, vector<vector<int>>&dp)
     {
@@ -52,6 +87,7 @@ public:
         int j=grid[0].size();
 
         // return usingRecursion(i-1, j-1, grid);   
-        return usingMemoization(i, j, grid); 
+        // return usingMemoization(i, j, grid); 
+        return usingTabulation(i, j, grid);
     }
 };
