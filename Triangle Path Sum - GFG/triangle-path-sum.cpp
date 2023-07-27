d1a7226b3079b559db0a4d47369ffc6cb1a8c72c
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-28 03:36:03 +0530

    Added solution - LeetHub

--- Triangle Path Sum - GFG/triangle-path-sum.cpp
+++ Triangle Path Sum - GFG/triangle-path-sum.cpp
@@ -5,6 +5,32 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+    // using tabulation
+    int usingTabulation(int n, vector<vector<int>>&triangle)
+    {
+    	// create a dp array
+    	vector<vector<int>>dp(n, vector<int>(n,-1));
+    
+    	// handling the last row
+    	for(int j=0;j<n;j++)
+    	{
+    		dp[n-1][j]=triangle[n-1][j];
+    	}
+    
+    	// filling the dp in bottom up fashion
+    	for(int i=n-2;i>=0;i--)
+    	{
+    		for(int j=0; j<=i; j++)
+    		{
+    			int south=triangle[i][j] + dp[i+1][j];
+    			int southEast=triangle[i][j] + dp[i+1][j+1];
+    
+    			dp[i][j]=min(south, southEast);
+    		}
+    	}
+    	return dp[0][0];
+    }
+  
     // using memoization
     int usingMemoizationHelper(int currRow, int currCol, int n, vector<vector<int>>&triangle, vector<vector<int>>&dp)
     {
@@ -52,7 +78,8 @@ class Solution {
     {
         // Code here
         // return usingRecursion(0, 0, n, triangle);
-        return usingMemoization(0, 0, n, triangle) ;
+        // return usingMemoization(0, 0, n, triangle) ;
+        return usingTabulation(n, triangle);
     }
 };
 
