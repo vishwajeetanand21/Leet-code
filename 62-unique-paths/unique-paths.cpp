1b6630de8b16db6d7d20983376bc33ace2f315bc
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-28 01:39:25 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (48.64%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,5 +1,34 @@
 class Solution {
 public:
+    // using tabulation
+    int usingTabulation(int n, int m)
+    {
+        vector<vector<int>>dp(n, vector<int>(m, -1));
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m; j++)
+            {
+                if(i==0 && j==0)
+                {
+                    dp[i][j]=1;
+                }
+                else
+                {
+                    int up=0;
+                    int left=0;
+
+                    if(i>0)
+                        up=dp[i-1][j];
+                    if(j>0)
+                        left=dp[i][j-1];
+
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
     // using memoization
     int usingMemoizationHelper(int i, int j, vector<vector<int>>&dp)
     {
@@ -51,7 +80,8 @@ public:
     int uniquePaths(int n, int m)
     {
         // return usingRecursion(n-1, m-1);
-        return usingMemoization(n, m);
+        // return usingMemoization(n, m);
+        return usingTabulation(n, m);
     }
 
 
