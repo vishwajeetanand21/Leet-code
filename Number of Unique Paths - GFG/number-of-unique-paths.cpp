330b1377f4b25e53665fb8819d74098277ae871f
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-27 23:37:24 +0530

    Added solution - LeetHub

---
+++ Number of Unique Paths - GFG/number-of-unique-paths.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+//Initial template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template in C++
+
+class Solution
+{
+    public:
+    //Function to find total number of unique paths.
+    // using memoization
+    int usingMemoizationHelper(int i, int j, vector<vector<int>>& dp) 
+    {
+        if (dp[i][j] != -1) 
+        {
+            return dp[i][j];
+        }
+    
+        // base case IMPORTANT
+        if (i == 1 && j == 1) 
+        {
+            return 1;
+        }
+        if (i <=0 || j <= 0) 
+        {
+            return 0;
+        }
+    
+        int up = usingMemoizationHelper(i - 1, j, dp);
+        int left = usingMemoizationHelper(i, j - 1, dp);
+    
+        return dp[i][j] = up + left;
+    }
+    int usingMemoization(int i, int j)
+    {
+        vector<vector<int>> dp(i+1, vector<int>(j+1, -1));
+        return usingMemoizationHelper(i, j, dp);
+    }
+    
+    // using recursion
+    int usingRecursion(int i, int j)
+    {
+        // base case
+        if(i==0 && j==0)
+        {
+            return 1;
+        }
+        if(i<0 || j<0)
+        {
+            return 0;
+        }
+        
+        int up=usingRecursion(i-1, j);
+        int left=usingRecursion(i, j-1);
+        
+        return up+left;
+    }
+    int NumberOfPath(int a, int b)
+    {
+        //code here
+        // return usingRecursion(a-1,b-1);
+        return usingMemoization(a, b);
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking dimensions of the matrix
+        int a,b;
+        cin>>a>>b;
+        Solution ob;
+        //calling NumberOfPath() function
+        cout << ob.NumberOfPath(a,b) << endl;
+    }
+}
+
+
+// } Driver Code Ends
