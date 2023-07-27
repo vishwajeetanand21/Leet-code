90636e3eb66dd1345e508a32afc1c81562b8fd4c
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-07-28 03:15:06 +0530

    Added solution - LeetHub

---
+++ Triangle Path Sum - GFG/triangle-path-sum.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // using memoization
+    int usingMemoizationHelper(int currRow, int currCol, int n, vector<vector<int>>&triangle, vector<vector<int>>&dp)
+    {
+    	// first write the base case
+    	if(currRow==n-1)
+    	{
+    		return triangle[currRow][currCol];
+    	} 
+    	// there is no boundary condition for this question
+    
+    	// now check for the dp array
+    	if(dp[currRow][currCol]!=-1)
+    	{
+    		return dp[currRow][currCol];
+    	}
+    
+    
+    	int south=triangle[currRow][currCol] + usingMemoizationHelper(currRow+1, currCol, n, triangle, dp);
+    	int southEast=triangle[currRow][currCol] + usingMemoizationHelper(currRow+1, currCol+1, n, triangle, dp);
+    
+    	return dp[currRow][currCol] = min(south, southEast);
+    }
+    int usingMemoization(int currRow, int currCol, int n, vector<vector<int>>&triangle)
+    {
+    	vector<vector<int>>dp(n, vector<int>(n,-1));
+    	return usingMemoizationHelper(currRow, currCol, n, triangle, dp);
+    }
+    
+    // using recursion
+    int usingRecursion(int currRow, int currCol,int n, vector<vector<int>>&triangle)
+    {
+    	// write the base case
+    	if(currRow==n-1)
+    	{
+    		return triangle[currRow][currCol];
+    	}
+    	// there is no boundary condition for this question
+    
+    	int south=triangle[currRow][currCol] + usingRecursion(currRow+1, currCol, n, triangle);
+    	int southEast=triangle[currRow][currCol] + usingRecursion(currRow+1, currCol+1, n, triangle);
+    
+    	return min(south, southEast);
+    }
+    int minimizeSum(int n, vector<vector<int>>& triangle) 
+    {
+        // Code here
+        // return usingRecursion(0, 0, n, triangle);
+        return usingMemoization(0, 0, n, triangle) ;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> triangle;
+
+        for (int i = 0; i < n; i++) {
+            vector<int> temp;
+            for (int j = 0; j <= i; j++) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            triangle.push_back(temp);
+        }
+        Solution obj;
+        cout << obj.minimizeSum(n, triangle) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
