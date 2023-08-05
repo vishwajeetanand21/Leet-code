c38f89755d785620cc83ac5d5f3fafccd54c6c2d
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-08-05 18:47:24 +0530

    Time: 15 ms (59.81%) | Memory: 8.3 MB (26.86%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
+    /*
+    LOGIC: There are two types of roman numbers
+            Type 1: IV this is 4
+            Type 2: VI this is 6
+
+    TYPE 1:     if the ith character is smaller than the (i+1)th character
+                then the number will be (i+1)th character - ith character
+                                        V-I
+                                        =5-1
+                                        =4
+            
+    TYPE 2:     if the ith character is larger than the (i+1)th character
+                then the number will be (i)th character + (i+1)th character
+                                        V+I
+                                        5+1
+                                        6
+    */
     int romanToInt(string str) 
     {
+        // Initialize the map
         map<char, int>myMap;
         myMap['I']=1;
         myMap['V']=5;
@@ -11,25 +29,35 @@ public:
         myMap['D']=500;
         myMap['M']=1000;
 
+        // calculate the size of the strign
         int n=str.size();
 
+        // variable to store the answer
         int ans=0;
 
+        // traverse throught the string (IMPORTANT: Move two index when there is TYPE 1 number)
         for(int i=0; i<n; )
         {
-            int temp;
+            int temp; // a temporary variable to store the current number
+
+            // TYPE 2
             if(i==n-1 || myMap[str[i]] >= myMap[str[i+1]])
             {
                 temp=myMap[str[i]];
                 i++;
             }
+
+            // TYPE 1
             else
             {
                 temp = myMap[str[i+1]] - myMap[str[i]];
                 i+=2;
             }
+
+            // finally add the temporary variable to the answer variable
             ans+=temp;
         }      
+
         return ans;
     }
 };
