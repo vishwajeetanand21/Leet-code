4208a6f8280a88fb949a84d37387d5857a76519e
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-08-07 02:23:55 +0530

    Added solution - LeetHub

--- Minimum number of Coins - GFG/minimum-number-of-coins.cpp
+++ Minimum number of Coins - GFG/minimum-number-of-coins.cpp
@@ -12,26 +12,36 @@ public:
     vector<int> minPartition(int amount)
     {
         // code here
+        
+        // array of denominations
         int arr[]={2000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+        // array size
         int n=10;
         
-        
+        // vector to store the answer
         vector<int>answer;
+        
+        // traversing the array of denominatios TC: O(10) which is equivalent to O(1)
         for(int i=0;i<10; i++)
         {
-            if(amount<=0)
+            if(amount<=0) //whenever the amount reaches to 0 or negative then break the loop
             {
                 break;
             }
             
+            // current coin
             int currentDenomination=arr[i];
+            
+            // there is a infinite supply of each denomination that's why there is a while loop
+            // which will run untill the amount >= current coin
             while(amount >= currentDenomination)
             {
-                amount -= currentDenomination;
-                answer.push_back(currentDenomination);
+                amount -= currentDenomination; //keep on updating the amount
+                answer.push_back(currentDenomination);// add that coin into the array
             }
         }
         
+        // finally return the array
         return answer;
     }
 };
