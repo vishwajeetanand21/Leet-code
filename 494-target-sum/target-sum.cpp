3116a849b755abc3f3ff08ecca7dcc49a6a07376
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-08-08 00:09:41 +0530

    Time: 269 ms (38.30%) | Memory: 9.1 MB (73.48%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,31 +1,67 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
-        for(int x: nums)
-            sum += x;
-        if (abs(target) > sum || (sum + target) % 2 != 0){ 
-            return 0;
-        }
-        int s1 = (sum + target)/2;
-        int ans = subsetCount(nums,s1);
-        return ans;
-    }
-    
-    int subsetCount(vector<int>& nums, int s1){
-        int n = nums.size();
-        int t[n+1][s1+1];
-        t[0][0] = 1;
-        for(int i=1;i<s1+1;i++)
-            t[0][i] = 0;
-        for(int i=1;i<n+1;i++)
-            for(int j=0;j<s1+1;j++){
-                if(nums[i-1] > j)
-                    t[i][j] = t[i-1][j];
-                else
-                    t[i][j] = t[i-1][j] + t[i-1][j-nums[i-1]];
-            }
+    /*
+        LOGIC: This question is exactly same as Partition with given difference question
+               In which we have to divide the array into two parts such that 
+               the difference of sum of both the arrays will be the target
+
+            Eg: [1,2,3,1] target=3
+            
+        CASE 1: arr1=[1,3,1]    arr2=[2]
+                sum1=5          sum2=2
+                sum1-sum2
+                5-2
+                =3=target
         
-        return t[n][s1];
+        CASE 2: arr1=[2,3]      arr2=[1,1]
+                sum1=5          sum2=2
+                sum1-sum2
+                5-2
+                3=target
+
+                Therefore there are 2 ways to divide the array such that the difference of sum of arrays will be equal to target
+                THIS QUESTION IS EXACTLY SAME AS PARTITION WITH GIVEN DIFFERENCE
+    */
+
+    int usingRecursion(int currIndex, vector<int>&arr, int target)
+    {
+        // base case
+        if(currIndex==0)
+        {
+            if(target==0 && arr[0]==0)
+                return 2;
+            if(target==0 || target==arr[0])
+                return 1;
+            else    
+                return 0;
+        }
+
+        // small calculation
+        int notTake=usingRecursion(currIndex-1, arr, target);
+
+        int take=0;
+        if(arr[currIndex]<=target)
+        {
+            take=usingRecursion(currIndex-1, arr, target-arr[currIndex]);
+        }
+
+        return take + notTake;
+    }
+    int findTargetSumWays(vector<int>& arr, int target) 
+    {
+        int n=arr.size();
+
+        int totSum = 0;
+        for(int i=0; i<arr.size();i++){
+            totSum += arr[i];
+        }
+        
+        //Checking for edge cases
+        if(totSum-target<0) return 0;
+        if((totSum-target)%2==1) return 0;
+
+        int s2 = (totSum-target)/2;
+
+        return usingRecursion(n-1, arr, s2);
     }
 };
