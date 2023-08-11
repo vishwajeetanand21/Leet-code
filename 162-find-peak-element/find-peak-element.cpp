774a7158b6542fe80083aa992d5911c73fe0049c
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-08-12 00:17:12 +0530

    Time: 3 ms (82.25%) | Memory: 8.9 MB (10.37%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& arr) 
+    {
+        int n=arr.size();
+
+        // edge cases: all the boundary elements
+        if(n==1)
+            return 0;
+        if(arr[0]>arr[1])
+            return 0;
+        if(arr[n-2]<arr[n-1])
+            return n-1;
+
+        int low=1;
+        int high=n-2;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+
+            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1])
+                return mid;
+            
+            if(arr[mid-1]<arr[mid])
+                low=mid+1;
+            
+            else
+                high=mid-1;
+        }
+        return -1;
+    }
+};
