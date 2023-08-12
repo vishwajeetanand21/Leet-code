500bd370d435c3065ed5a162502ca60c01cdf467
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-08-12 23:21:27 +0530

    Time: 6 ms (58.49%) | Memory: 12.5 MB (6.29%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
@@ -1,5 +1,70 @@
 class Solution {
 public:
+
+		/*
+	                                             NORMAL METHOD
+	                
+	   A=36
+	   B=60
+	   
+	   STEP 1: Find all the factors of A and B
+	   A=2x2x3x3
+	   B=2x2x3x5
+	   
+	   STEP 2: Find all the common factors
+	   A=[2]x[2]x[3]x3
+	   B=[2]x[2]x[3]x5
+	   
+	   STEP 3: Do the multiplication of all the factors of A and B
+	   2x2x3
+	   12 
+	   Therefore the GCD/HCF of 36 and 60 is 12
+	   
+	   
+	   
+	   
+	                                             ECULIDEAN ALGORITHM
+	   METHOD 1: Using Subtraction
+	   
+	   We subtract smaller number from the larger number untill mini not becomes 0
+	   
+	   A=36 (mini)
+	   B=60 (maxi)
+	   
+	   maxi-mini=subtractedValue
+	   
+	   60-36=24        {subtract mini from maxi number}
+	   36-24=12        {now interchange the value, maxi=mini and mini=subtracted value}
+	   24-12=12 
+	   12-12=0
+	   12-0=12
+	   
+	   At last if the value of mini=0 then our answer will be stored at maxi
+	   TC: O(min(a,b))
+	   
+	   
+	   
+	   
+	   METHOD 2: Using remainder
+	   
+	   We divide larger number from smaller number untill mini not becomes 0
+	   
+	   A=36  (mini)
+	   B=60  (maxi)
+	   
+	   maxi%mini=remainder
+	   60%36=24         {Divide maxi from mini and get the remainder}
+	   36%24=12         {now interchange the value maxi=mini and mini=remainder}
+	   24%12=0
+	   12%0=0
+	   
+	   At last if the value of mini=0 then our answer will be stored at maxi
+	   TC: O(log(min(a,b)))
+	*/
+
+
+
+
     // using recursion
     int GCD2(int a, int b)
     {
