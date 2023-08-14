88bb7967d84f489c24a26b4740a9a7a4a13453ff
vishwajeetanand21 <81609398+vishwajeetanand21@users.noreply.github.com>
2023-08-14 23:27:44 +0530

    Added solution - LeetHub

--- Pattern-1 - GFG/pattern1.cpp
+++ Pattern-1 - GFG/pattern1.cpp
@@ -21,6 +21,7 @@ class Solution {
             string temp="";
             for(int j=1;j<=N;j++)
             {
+                // boundary condition
                 if(i==1 || i==N || j==1 || j==N)
                 {
                     temp.push_back(counter);
